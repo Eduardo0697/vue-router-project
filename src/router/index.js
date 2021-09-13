e4806fe85de383cc9e28e6c5679d0fb2c1338031
Eduardo Hernandez <eduardo0697@armandoschmitt.de>
2021-09-13 12:28:21 -0500

    Update navigation router links

--- src/router/index.js
+++ src/router/index.js
@@ -8,43 +8,13 @@ const routes = [
     component: Home,
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
-  },
-  {
-    path: "/brazil",
-    name: "Brazil",
-    component: () =>
-      import(/* webpackChunkName: "brazil" */ "../views/Brazil.vue"),
-  },
-  {
-    path: "/panama",
-    name: "Panama",
-    component: () =>
-      import(/* webpackChunkName: "panama" */ "../views/Panama.vue"),
-  },
-  {
-    path: "/jamaica",
-    name: "Jamaica",
-    component: () =>
-      import(/* webpackChunkName: "jamaica" */ "../views/Jamaica.vue"),
-  },
-  {
-    path: "/hawaii",
-    name: "Hawaii",
-    component: () =>
-      import(/* webpackChunkName: "hawaii" */ "../views/Hawaii.vue"),
-  },
-  {
     // You only need to change the attribute path to change the name in all the routes
-    path: "/places-to-visit/:id",
+    path: "/places-to-visit/:id/:slug",
     name: "destination.show",
-    component: () => import("../views/DestinationShow.vue"),
+    component: () =>
+      import(
+        /* webpackChunkName: "destination" */ "../views/DestinationShow.vue"
+      ),
   },
 ];
 
