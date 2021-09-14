0d33e0ef6e25755eca5c23904092e42a55a56e71
Eduardo Hernandez <eduardo0697@armandoschmitt.de>
2021-09-14 14:58:05 -0500

    Make ExperienceShow page to recover the data from api

--- src/views/ExperienceShow.vue
+++ src/views/ExperienceShow.vue
@@ -1,5 +1,5 @@
 <template>
-  <section>
+  <section v-if="destination">
     <h1>{{ experience.name }}</h1>
     <img :src="`/images/${experience.image}`" :alt="experience.name" />
     <p>{{ experience.description }}</p>
@@ -7,23 +7,41 @@
 </template>
 
 <script>
-import sourceData from "../data.json";
+// import sourceData from "../data.json";
 export default {
   props: {
     id: { type: Number, required: true },
     experienceSlug: { type: String, required: true },
+    slug: { type: String },
+  },
+  data() {
+    return {
+      destination: null,
+    };
   },
   computed: {
-    destination() {
-      return sourceData.destinations.find(
-        (destination) => destination.id === this.id
-      );
-    },
+    // destination() {
+    //   return sourceData.destinations.find(
+    //     (destination) => destination.id === this.id
+    //   );
+    // },
     experience() {
       return this.destination.experiences.find(
         (experience) => experience.slug === this.experienceSlug
       );
     },
+  },
+  methods: {
+    async initData() {
+      // https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+      const response = await fetch(
+        `https://travel-dummy-api.netlify.app/${this.slug}.json`
+      );
+      this.destination = await response.json();
+    },
+  },
+  async created() {
+    this.initData();
   },
 };
 </script>
