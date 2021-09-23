9b7019763a447b69a62c64c05937b840be18dcf4
Eduardo Hernandez <eduardo0697@armandoschmitt.de>
2021-09-23 18:54:14 -0500

    Extend the router link to manage internal urls and external urls

--- src/components/TheNavigation.vue
+++ src/components/TheNavigation.vue
@@ -1,22 +1,25 @@
 <template>
   <div id="nav">
-    <router-link id="logo" to="/">Vue School Travel</router-link>
-    <router-link
+    <AppLink id="logo" to="/">Vue School Travel</AppLink>
+    <AppLink
       v-for="destination in destinations"
       :key="destination.id"
       :to="{ name: 'destination.show', params: { id: destination.id , slug: destination.slug} }"
     >
       {{ destination.name }}
-    </router-link>
-    <router-link :to="{ name: 'protected' }">Dashboard</router-link>
+    </AppLink>
+    <AppLink :to="{ name: 'protected' }">Dashboard</AppLink>
+    <AppLink to="https://vueschool.io">Vue School</AppLink>
   </div>
 </template>
 
 <script>
 import sourceData from "../data.json";
+import AppLink from "./AppLink";
 
 export default {
   name: "TheNavigation",
+  components: {AppLink},
   data() {
     return {
       destinations: sourceData.destinations,
--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+// Declaring it here we avoid to use it in each Vue component
+import AppLink from "./components/AppLink";
 
-createApp(App).use(router).mount("#app");
+createApp(App)
+  .component("AppLink", AppLink)
+  .use(router)
+  .mount("#app");
